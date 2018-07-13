85c08361421baba980a8651e4d760d32c6793ad0
Joao Vitor BF <joaovitorbf@protonmail.com>
2018-07-13 14:47:02 -0300

    Fix

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QSoundEffect>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    QSoundEffect voice;
-    voice.setSource(QUrl::fromLocalFile(":/sounds/voice.wav"));
-    voice.setVolume(0.02f);
-    voice.play();
-
 
     return a.exec();
 }
